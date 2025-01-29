e1b04855ae37c486bce799c7cba5743f70470b9b
lucia294 <cecifv7138@gmail.com>
2025-01-29 21:06:08 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import Vuelos from './Vuelos'; // Importamos Vuelos para verificar si se renderiza.
 
-test('debería renderizar el componente Vuelos dentro de App', () => {
+test('debería renderizar el componente time dentro de App', () => {
   render(<App />);
 
-  // Comprobamos que el componente Vuelos se renderiza
-  const vuelosElement = screen.getByText(/time/i); // texto que está dentro de Vuelos.js
+  // Comprobamos que el componente time se renderiza
+  const vuelosElement = screen.getByText(/time/i); 
   expect(vuelosElement).toBeInTheDocument();
 });
 
